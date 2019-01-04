f2714e056fdca2746db5e1fbb64d62d9d4d00b79
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-01-04 11:39:22 +0000

    adding input

--- directory.rb
+++ directory.rb
@@ -1,21 +1,36 @@
 student_count = 11
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+# students = [
+#   {name: "Dr. Hannibal Lecter", cohort: :november},
+#   {name: "Darth Vader", cohort: :november},
+#   {name: "Nurse Ratched", cohort: :november},
+#   {name: "Michael Corleone", cohort: :november},
+#   {name: "Alex DeLarge", cohort: :november},
+#   {name: "The Wicked Witch of the West", cohort: :november},
+#   {name: "Terminator", cohort: :november},
+#   {name: "Freddy Krueger", cohort: :november},
+#   {name: "The Joker", cohort: :november},
+#   {name: "Joffrey Baratheon", cohort: :november},
+#   {name: "Norman Bates", cohort: :november}
+# ]
 
 def print_header
 puts "The students of Villains Academy"
 puts "-------------"
+end
+
+def input_students
+  puts "Please enter name of student"
+  puts 'To finish, just hit return twice'
+  students = []
+
+  name = gets.chomp
+
+  while !name.empty? do
+  students << {name: name, cohort: :november}
+  puts "Now we have #{students.count} students"
+  name = gets.chomp
+  end
+  students
 end
 
 def print_names(students)
@@ -28,6 +43,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end 
 
+students = input_students
 print_header
 print_names(students)
 print_footer(students)
